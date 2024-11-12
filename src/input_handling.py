2fc779ee6546f6120f5d7d0d76442ff59b30c1ce
Josef Procházka <jos.prochazka@post.cz>
2024-11-12 11:44:06 +0100

    Refactor to use crawlee (#23)
    Remove custom functions and use generic functions from Crawlee instead.
    
    Use pydantic for Actor input data.
    Remove  input: soupFromEncoding, soupExcludeEncodings
    Update documentation.

---
+++ src/input_handling.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+import re
+from datetime import timedelta
+from re import Pattern
+from typing import Callable, Sequence, cast
+
+from crawlee import Glob
+from crawlee.beautifulsoup_crawler import BeautifulSoupParser
+from pydantic import BaseModel, ConfigDict, Field
+
+from apify import Actor, ProxyConfiguration
+from src.utils import USER_DEFINED_FUNCTION_NAME
+
+
+class ActorInputData(BaseModel):
+    """Processed and cleaned inputs for the actor."""
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    start_urls: Sequence[str]
+    link_selector: str = ''
+    link_patterns: list[Pattern | Glob] = []
+    max_depth: int = Field(0, ge=0)
+    request_timeout: timedelta = Field(timedelta(seconds=10), gt=timedelta(seconds=0))
+    proxy_configuration: ProxyConfiguration
+    soup_features: BeautifulSoupParser
+    user_function: Callable
+
+    @classmethod
+    async def from_input(cls) -> ActorInputData:
+        """Instantiate the class from Actor input."""
+        actor_input = await Actor.get_input() or {}
+
+        if not (start_urls := actor_input.get('startUrls', [])):
+            Actor.log.error('No start URLs specified in actor input, exiting...')
+            await Actor.exit(exit_code=1)
+
+        if not (page_function := actor_input.get('pageFunction', '')):
+            Actor.log.error('No page function specified in actor input, exiting...')
+            await Actor.exit(exit_code=1)
+
+        if (
+            proxy_configuration := await Actor.create_proxy_configuration(
+                actor_proxy_input=actor_input.get('proxyConfiguration')
+            )
+        ) is not None:
+            aid = cls(
+                start_urls=[start_url['url'] for start_url in start_urls],
+                link_selector=actor_input.get('linkSelector', ''),
+                link_patterns=[
+                    re.compile(pattern) for pattern in actor_input.get('linkPatterns', ['.*'])
+                ],  # default matches everything
+                max_depth=actor_input.get('maxCrawlingDepth', 1),
+                request_timeout=timedelta(seconds=actor_input.get('requestTimeout', 10)),
+                proxy_configuration=proxy_configuration,
+                soup_features=actor_input.get('soupFeatures', 'html.parser'),
+                user_function=await extract_user_function(page_function),
+            )
+        else:
+            Actor.log.error('Creation of proxy configuration failed, exiting...')
+            await Actor.exit(exit_code=1)
+
+        Actor.log.debug(f'actor_input = {aid}')
+
+        return aid
+
+
+async def extract_user_function(page_function: str) -> Callable:
+    """Extract the user-defined function using exec and returns it as a Callable.
+
+    This function uses `exec` internally to execute the `user_function` code in a separate scope. The `user_function`
+    should be a valid Python code snippet defining a function named `USER_DEFINED_FUNCTION_NAME`.
+
+    Args:
+        page_function: The string representation of the user-defined function.
+
+    Returns:
+        The extracted user-defined function.
+
+    Raises:
+        KeyError: If the function name `USER_DEFINED_FUNCTION_NAME` cannot be found.
+    """
+    scope: dict = {}
+    exec(page_function, scope)
+
+    try:
+        user_defined_function = scope[USER_DEFINED_FUNCTION_NAME]
+    except KeyError:
+        Actor.log.error(f'Function name "{USER_DEFINED_FUNCTION_NAME}" could not be found, exiting...')
+        await Actor.exit(exit_code=1)
+
+    return cast(Callable, user_defined_function)
--- src/utils.py
+++ src/utils.py
@@ -1,131 +1,17 @@
 from __future__ import annotations
 
-import re
 from inspect import iscoroutinefunction
-from typing import TYPE_CHECKING, Callable, cast
-from urllib.parse import urljoin
+from typing import TYPE_CHECKING, Callable
 
 from apify import Actor
 
-from .dataclasses import Context
-
 if TYPE_CHECKING:
-    from bs4 import BeautifulSoup
-
-    from apify.storages import RequestQueue
-
+    from crawlee.beautifulsoup_crawler import BeautifulSoupCrawlingContext
 
 USER_DEFINED_FUNCTION_NAME = 'page_function'
 
 
-async def get_proxies_from_conf(proxy_configuration: dict | None) -> dict | None:
-    """Retrieve the proxies dictionary based on the provided proxy configuration.
-
-    Args:
-        proxy_configuration: The proxy configuration dictionary. If None, no proxies will be used.
-
-    Returns:
-        The proxies dictionary containing "http" and "https" keys with the proxy URL.
-        Returns None if proxy_configuration is None.
-    """
-    if proxy_configuration:
-        conf = await Actor.create_proxy_configuration(actor_proxy_input=proxy_configuration)
-
-        if conf is None:
-            Actor.log.error('Creation of proxy configuration failed, exiting...')
-            await Actor.exit(exit_code=1)
-        else:
-            proxy_url = await conf.new_url()
-            return {'http://': proxy_url, 'https://': proxy_url}
-
-    return None
-
-
-async def update_request_queue(
-    soup: BeautifulSoup,
-    request_queue: RequestQueue,
-    request: dict,
-    max_depth: int,
-    link_selector: str,
-    link_patterns: list[str],
-) -> None:
-    """Update the request queue with new links found in the response.
-
-    This function parses the HTML content of the response using BeautifulSoup and extracts links based
-    on the provided CSS selector. It then checks each link against the specified regex patterns to determine
-    if it should be enqueued in the request queue. The depth of the links is compared to the maximum depth
-    to avoid exceeding it. Valid links are enqueued with an increased depth in the request queue.
-
-    Args:
-        soup: The BeautifulSoup object to get links from.
-        request_queue: The request queue to update.
-        request: The original request.
-        max_depth: The maximum depth allowed for enqueueing links.
-        link_selector: The CSS selector to locate the links in the HTML content.
-        link_patterns: The list of regex patterns to match the links against.
-
-    Returns:
-        None
-    """
-    url = request['url']
-    depth = request['userData']['depth']
-
-    if depth >= max_depth:
-        return
-
-    items = soup.select(link_selector)
-
-    # If we haven't reached the max depth, look for nested links and enqueue their targets
-    for item in items:
-        link_url = urljoin(url, item['href'])
-
-        # Regex matching
-        matched = False
-        for pattern in link_patterns:
-            if re.match(pattern, link_url):
-                matched = True
-                break
-
-        if not matched:
-            Actor.log.debug(f'Link URL {link_url} does not match any pattern')
-            continue
-
-        if not link_url.startswith(('http://', 'https://')):
-            Actor.log.debug(f'Link URL {link_url} does not start with http/https')
-            continue
-
-        Actor.log.info(f'Enqueuing {link_url} ...')
-        await request_queue.add_request(request={'url': link_url, 'userData': {'depth': depth + 1}})
-
-
-async def extract_user_function(page_function: str) -> Callable:
-    """Extract the user-defined function using exec and returns it as a Callable.
-
-    This function uses `exec` internally to execute the `page_function` code in a separate scope. The `page_function`
-    should be a valid Python code snippet defining a function named `USER_DEFINED_FUNCTION_NAME`.
-
-    Args:
-        page_function: The string representation of the user-defined function.
-
-    Returns:
-        The extracted user-defined function.
-
-    Raises:
-        KeyError: If the function name `USER_DEFINED_FUNCTION_NAME` cannot be found.
-    """
-    scope: dict = {'Context': Context}
-    exec(page_function, scope)
-
-    try:
-        user_defined_function = scope[USER_DEFINED_FUNCTION_NAME]
-    except KeyError:
-        Actor.log.error(f'Function name "{USER_DEFINED_FUNCTION_NAME}" could not be found, exiting...')
-        await Actor.exit(exit_code=1)
-
-    return cast(Callable, user_defined_function)
-
-
-async def execute_user_function(context: Context, user_defined_function: Callable) -> None:
+async def execute_user_function(context: BeautifulSoupCrawlingContext, user_defined_function: Callable) -> None:
     """Execute the user-defined function with the provided context and pushes data to the Actor.
 
     This function checks if the provided user-defined function is a coroutine. If it is, the function is awaited.
